76b27a41dc95a64e2411ff31b62e63bf6be74a62
Jakub Standarski <busketz2k@gmail.com>
2018-12-15 17:47:44 +0100

    Modified: Size of pointer variables of different type added

--- Pointers/pointer_basics.c
+++ Pointers/pointer_basics.c
@@ -10,24 +10,29 @@ int main()
     double d=2.31, *dp=&d;
 
     printf("\n\n");
-    printf(" Value of ip = address of i = %u\n", ip);   // ip stores the address of i
-    printf(" Value of dp = address of d = %u\n", dp);   // dp stores the address d
+    printf(" Value of ip = address of i = %p\n", ip);   // ip stores the address of i
+    printf(" Value of dp = address of d = %p\n", dp);   // dp stores the address d
 
     printf("\n\n");
     printf(" Value at address stored in ip = value of i = %d\n", *ip); // *ip dereferences to value at address stored in ip 
     printf(" Value at address stored in dp = value of d = %f\n", *dp); // *dp dereferences to value at address stored in dp
 
     printf("\n\n");
-    printf(" Address of ip = %u\n", &ip); // address of pointer ip
-    printf(" Address of dp = %u\n", &dp); // address of pointer dp
+    printf(" Address of ip = %p\n", &ip); // address of pointer ip
+    printf(" Address of dp = %p\n", &dp); // address of pointer dp
     
     /*
      *   Memory occupied by pointer variables
      *   is same regardless of its base type
      */
+
+    char *ch;
+    int *it;
+    float *ft;
+    double *db;
     printf("\n\n");
-    printf(" Size of pointer ip = %d\n", sizeof(ip));
-    printf(" Size of pointer dp = %d\n", sizeof(dp));
+    printf(" Size of pointer variables of type: char = %lu, int = %lu, float = %lu, double = %lu (bytes)\n", sizeof(ch), sizeof(it), sizeof(ft), sizeof(db));
+    printf(" Size of variables of type: char = %lu, int = %lu, float = %lu, double = %lu (bytes)\n", sizeof(*ch), sizeof(*it), sizeof(*ft), sizeof(*db));
 
     // Signal to operating system that program ran fine
     return 0;
